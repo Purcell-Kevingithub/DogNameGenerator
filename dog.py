0af26d850d24432c76cd7fa54970f8174788135c
Kevin Purcell <kevin91purcell@gmail.com>
2018-07-31 11:57:31 -0400

    added list of dog names

---
+++ dog.py
@@ -0,0 +1,30 @@
+Balto
+Beatrice
+Beethoven
+Benji
+Bingo
+Bolt
+Buddy
+Butch
+Chance
+Charlie
+Copper
+Cujo
+Fang
+Gus
+Hachi
+Hooch
+Hubert
+Lady
+Lassie
+Marley
+Marmaduke
+Miss Agnes
+Old Yeller
+Pencil
+Pongo
+Scooby
+Shadow
+Shiloh
+Winky
+Winn - Dixie
